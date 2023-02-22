7d8cd61b6b30e6b87cfeb6ad1687227739e17ef7
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-02-22 11:39:27 -0600

    El codigo se a terminado

---
+++ EJERCICIOJBHZ22022223/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.Write("======");
+Console.Write("BIENVENIDO USUARIO INTELIGENTE");
+Console.Write("======");
+Console.WriteLine();
+
+Console.WriteLine("Porfavor ingrese valor incial con el cual desea comenzar:");
+int ValorInicial = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Porfavor ingrese valor final con el cual desea terminar:");
+int ValorFinal = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Usuario de los valores que ingreso en  valor incial y valor final [{0},{1}]." ,ValorInicial,ValorFinal);
+Console.WriteLine(); 
+Console.WriteLine("Se obtuvo de los siguientes numeros pares:");
+
+
+for (int i = ValorInicial; i < ValorFinal; i++)
+{if (i % 2 == 0)
+{
+Console.WriteLine("{0}", i);
+}
+}
+
+ 
+
+
+
--- EJERCICIOJBHZ22022223/obj/Debug/net6.0/EJERCICIOJBHZ22022223.AssemblyInfo.cs
+++ EJERCICIOJBHZ22022223/obj/Debug/net6.0/EJERCICIOJBHZ22022223.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("EJERCICIOJBHZ2202223")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EJERCICIOJBHZ22022223")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("EJERCICIOJBHZ2202223")]
-[assembly: System.Reflection.AssemblyTitleAttribute("EJERCICIOJBHZ2202223")]
+[assembly: System.Reflection.AssemblyProductAttribute("EJERCICIOJBHZ22022223")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EJERCICIOJBHZ22022223")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
